f6dd2ee34404fba47e56bd0b8baf268809aae539
pjch8908 <75929653+pjch8908@users.noreply.github.com>
2021-02-08 14:56:45 +0900

    js-basics #3.8

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "33bf64520ce50a9d40dfddcbaf764f39";
+
 const COORDS = "coords";
 
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
 function handleGeoSucess(position) {
-  console.log(position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
 }
 
 function handleGeoError() {
